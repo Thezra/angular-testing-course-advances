a9c068dc93e4b320c6e0af646ed251e7a9acb9d0
Isabela Luján Jaramillo <ilujanj@unal.edu.co>
2022-04-26 16:34:56 -0500

    Section 1 done

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -11,7 +12,15 @@ describe('CalculatorService', () => {
         console.log("Calling beforeEach");
 
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-        calculator = new CalculatorService(loggerSpy);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculator = TestBed.get(CalculatorService);
     });
 
     it('should add two numbers', () => {
