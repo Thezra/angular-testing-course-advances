4732f34346786c86b4663946215b56c8b92a135f
Isabela Luján Jaramillo <ilujanj@unal.edu.co>
2022-05-18 10:25:21 -0500

    Clase 15 terminada

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 import { CoursesService } from "./courses.service";
 
 describe("CourseServie", () => {
@@ -45,6 +47,46 @@ describe("CourseServie", () => {
         const req = httpTestingController.expectOne('/api/courses/12');
         expect(req.request.method).toEqual("GET");
         req.flush((COURSES[12]));
+    });
+
+    it('Should save the course data', () => {
+        const changes: Partial<Course> = {titles:{description: 'Testing Course'}}
+
+        coursesService.saveCourse(12, changes)
+            .subscribe(course => {
+                expect(course.id).toBe(12);
+            });
+
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual("PUT");
+        expect(req.request.body.titles.description)
+            .toEqual(changes.titles.description);
+
+        req.flush({
+            ...COURSES[12],
+            ...changes
+        });
+    })
+
+    it('Should give an error if save course fails', () => {
+        const changes: Partial<Course> = {titles:{description: 'Testing Course'}};
+
+        coursesService.saveCourse(12, changes)
+            .subscribe(
+                () => fail("The save course operation should have failed"),
+                (error: HttpErrorResponse) => {
+                    expect(error.status).toBe(500);
+                }
+            );
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual("PUT");
+
+        req.flush('Save course failed', {
+            status:500,
+            statusText: 'Internal Server Error'
+        });
     });
 
     afterEach(() => {
